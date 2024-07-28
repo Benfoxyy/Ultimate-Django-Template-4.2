f5f5458dc97c3e98c903336f3eaed1a2285d28ca
Ben <146076866+Benfoxyy@users.noreply.github.com>
2024-07-28 17:54:46 +0330

    !

--- core/core/settings.py
+++ core/core/settings.py
@@ -136,11 +136,11 @@ USE_TZ = True
 STATIC_URL = "static/"
 MEDIA_URL = "media/"
 
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_ROOT = BASE_DIR / "media"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles",
+    BASE_DIR / "static",
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
